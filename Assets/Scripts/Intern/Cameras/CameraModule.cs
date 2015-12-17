94ca097d9a66d73abb5f496c14289c4c1d52ae15
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2015-12-17 16:51:25 +0100

    first version of Character class
    first version of Unit class
    begin implementation of CameraModule class
    Template.cs modified

--- Assets/Scripts/Intern/Cameras/CameraModule.cs
+++ Assets/Scripts/Intern/Cameras/CameraModule.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// @author : 
+
+using UnityEngine;
+using System.Collections;
+
+namespace Extinction
+{
+    namespace Cameras
+    {
+        public abstract class CameraModule : MonoBehaviour
+        {
+            // ----------------------------------------------------------------------------
+            // -------------------------------- ATTRIBUTES --------------------------------
+            // ----------------------------------------------------------------------------
+
+            protected Vector3 _position;
+            protected Vector3 _rotation;
+            protected float _fieldOfView;
+
+            // ----------------------------------------------------------------------------
+            // --------------------------------- METHODS ----------------------------------
+            // ----------------------------------------------------------------------------
+
+            public abstract void setPosition( Vector3 position );
+            public abstract void setRotation( Vector3 rotation );
+            public abstract void setFieldOfView( float fieldOfView );
+            public abstract void zoom( float targetFieldOfView, float time );
+        }
+    }
+}
