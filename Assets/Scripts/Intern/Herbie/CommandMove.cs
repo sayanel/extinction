047179737e1fcdeef19e05f974b9e97a09d2863a
florian <florian.lequertier@free.fr>
2015-12-19 21:50:29 +0100

    Split implementations of commands into separated files.

--- Assets/Scripts/Intern/Herbie/CommandMove.cs
+++ Assets/Scripts/Intern/Herbie/CommandMove.cs
@@ -1,1 +1,68 @@
-﻿
+﻿// @author : florian
+
+using UnityEngine;
+using System.Collections;
+
+using Extinction.Characters;
+
+namespace Extinction
+{
+    namespace Herbie
+    {
+
+        /// <summary>
+        /// Command implementation, to move the agent to a position
+        /// </summary>
+        public class CommandMove : Command
+        {
+
+            // ----------------------------------------------------------------------------
+            // -------------------------------- ATTRIBUTES --------------------------------
+            // ----------------------------------------------------------------------------
+
+            /// <summary>
+            /// The unit which will follow the order
+            /// </summary>
+            private SpecialRobot _actor;
+
+            private Vector3 _targetPosition;
+
+            // ----------------------------------------------------------------------------
+            // --------------------------------- METHODS ----------------------------------
+            // ----------------------------------------------------------------------------
+
+            /// <summary>
+            /// create the command with all the informations : 
+            /// </summary>
+            public CommandMove( SpecialRobot actor, Vector3 targetPosition )
+            {
+                _actor = actor;
+                _targetPosition = targetPosition;
+            }
+
+            public override void Execute()
+            {
+                _actor.UnitBehaviour = UnitBehaviour.MOVE;
+
+                _actor.move( _targetPosition );
+            }
+
+            /// <summary>
+            /// Finished when the agent is near the targeted position
+            /// </summary>
+            public override bool IsFinished()
+            {
+                //if the agent is near the targeted position 
+                if( Vector3.SqrMagnitude( _actor.transform.position - _targetPosition ) < 1 )
+                    return true;
+                else
+                    return false;
+            }
+
+            public override void End()
+            {
+                //nothing
+            }
+        }
+    }
+}
--- Assets/Scripts/Intern/Herbie/CommandMoveAndAttack.cs
+++ Assets/Scripts/Intern/Herbie/CommandMoveAndAttack.cs
@@ -1,1 +1,78 @@
-﻿
+﻿// @author : florian
+
+using UnityEngine;
+using System.Collections;
+
+using Extinction.Characters;
+
+namespace Extinction
+{
+    namespace Herbie
+    {
+
+        /// <summary>
+        /// Command implementation, so that the agent attack the target.
+        /// </summary>
+        public class CommandMoveAndAttack : Command
+        {
+
+            private SpecialRobot _actor;
+            private Character _target;
+            private bool _isFinished = false;
+            //the delay between two update
+            private float _aiDelay = 1;
+            //ptr to coroutine to properly stop it
+            private IEnumerator _moveAndAttackCoroutine;
+
+            //create the command with all the informations : 
+            public CommandMoveAndAttack( SpecialRobot actor, Character target, float aiDelay = 1 )
+            {
+                _actor = actor;
+                _target = target;
+                _aiDelay = aiDelay;
+            }
+
+            public override void Execute()
+            {
+                _actor.UnitBehaviour = UnitBehaviour.ATTACK;
+
+                _moveAndAttackCoroutine = MoveAndAttackRoutine();
+                _actor.StartCoroutine( _moveAndAttackCoroutine );
+            }
+
+            //finished when the target has been killed, or when the target has escaped
+            public override bool IsFinished()
+            {
+                return _isFinished;
+            }
+
+            IEnumerator MoveAndAttackRoutine()
+            {
+                while( !_isFinished )
+                {
+                    if( !_target.IsAlive ) // is the target already dead ? 
+                    {
+                        _isFinished = true;
+                    }
+                    //Check if agent can attack
+                    else if( _actor.canAttack( _target ) )
+                    {
+                        _actor.stopWalking();
+                        _actor.attack( _target );
+                    }
+                    else
+                    {
+                        _actor.move( _target.transform.position );
+                    }
+
+                    yield return new WaitForSeconds( _aiDelay );
+                }
+            }
+
+            public override void End()
+            {
+                _actor.StopCoroutine( _moveAndAttackCoroutine );
+            }
+        }
+    }
+}
