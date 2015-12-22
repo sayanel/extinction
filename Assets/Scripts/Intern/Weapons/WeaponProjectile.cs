d1effa689fbb5d5a318d46da93d745bb057d7daf
Hisuuu <hp.hisu@gmail.com>
2015-12-22 16:09:24 +0100

    Weapons

--- Assets/Scripts/Intern/Weapons/WeaponProjectile.cs
+++ Assets/Scripts/Intern/Weapons/WeaponProjectile.cs
@@ -1,1 +1,56 @@
-﻿
+﻿// @author : Pascale
+
+using UnityEngine;
+using System.Collections;
+
+namespace Extinction
+{
+    namespace Weapons
+    {
+
+        public class WeaponProjectile : Weapon
+        {
+            // ----------------------------------------------------------------------------
+            // -------------------------------- ATTRIBUTES --------------------------------
+            // ----------------------------------------------------------------------------
+            protected Projectile _projectile;
+            protected float _velocity = 10f;
+            protected Transform _anchor;
+            protected string[] _targetTag;
+            // ----------------------------------------------------------------------------
+            // --------------------------------- METHODS ----------------------------------
+            // ----------------------------------------------------------------------------
+            public void Start()
+            {
+                _previousTime = Time.time;
+                if(_anchor == null && transform.childCount > 0){
+                    _anchor = transform.GetChild(0);
+                }
+            }
+
+            override
+            public void fire()
+            {
+                if (Time.time - _previousTime >= _fireRate)
+                {
+                    Projectile p = Instantiate(_projectile, _anchor.position, _anchor.rotation) as Projectile;
+                    p.Dammage = _dammage;
+                    p.TargetTag = _targetTag;
+                    Rigidbody pBody = p.GetComponent<Rigidbody>();
+                    if (pBody != null)
+                        pBody.AddForce(_anchor.forward * _velocity);
+                    _previousTime = Time.time;
+                    _nbCurrentAmmo--;
+                }
+            }
+
+            override
+            public void reload(int ammo)
+            {
+                int nb = _magazineMaxCapacity - _nbCurrentAmmo;
+                _nbCurrentAmmo += (ammo > nb) ? nb : ammo;
+            }
+        
+        }
+    }
+}
--- Assets/Scripts/Intern/Weapons/WeaponRay.cs
+++ Assets/Scripts/Intern/Weapons/WeaponRay.cs
@@ -1,1 +1,72 @@
-﻿
+﻿// @author : Pascale
+
+using UnityEngine;
+using System.Collections;
+using Extinction.Characters;
+
+namespace Extinction
+{
+    namespace Weapons
+    {
+
+        public class WeaponRay : Weapon, IHitTarget
+        {
+
+            // ----------------------------------------------------------------------------
+            // -------------------------------- ATTRIBUTES --------------------------------
+            // ----------------------------------------------------------------------------
+            protected float _minDistance = 1.0f;
+            protected float _maxDistance = 100f;
+            protected float _rayLenght = 100f;
+            protected Transform _anchor;
+            protected string[] _targetTag;
+
+
+            // ----------------------------------------------------------------------------
+            // --------------------------------- METHODS ----------------------------------
+            // ----------------------------------------------------------------------------
+            public void Start()
+            {
+                _previousTime = Time.time;
+                _maxDistance = _rayLenght + _minDistance;
+                if (_anchor == null && transform.childCount > 0)
+                {
+                    _anchor = transform.GetChild(0);
+                }
+            }
+
+            override
+            public void fire() {
+                if (Time.time - _previousTime >= _fireRate)
+                {
+                    RaycastHit hitInfo;
+                    if(Physics.Raycast(_anchor.position + _anchor.forward * _minDistance, 
+                                        _anchor.forward,
+                                        out hitInfo,
+                                        _rayLenght,
+                                        LayerMask.GetMask(_targetTag))){
+                        GameObject target = hitInfo.transform.gameObject;
+                        onHit(target);
+                    }
+                    _nbCurrentAmmo--;
+                }
+                _previousTime = Time.time;
+            }
+
+            public void onHit(GameObject obj)
+            {
+                Character o = obj.GetComponent<Character>();
+                o.getDamage(_dammage);
+            }
+
+            override
+            public void reload(int ammo)
+            {
+                int nb = _magazineMaxCapacity - _nbCurrentAmmo;
+                _nbCurrentAmmo += (ammo > nb) ? nb : ammo;
+            }
+
+
+        }
+    }
+}
