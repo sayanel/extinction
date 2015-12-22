d85a13b9e14bf87bc8de2b804196126f1599cd8e
Alex <alexis.oblet@gmail.com>
2015-12-22 17:10:45 +0100

    Players prefs: nickname get set

---
+++ Assets/Scripts/Intern/UI/MainMenu/InitNickname.cs
@@ -0,0 +1,16 @@
+﻿// @author : Alex
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Extinction {
+    namespace UI {
+        public class InitNickname : MonoBehaviour {
+
+            public void Start() {
+                GetComponent<InputField>().text = NicknameUtil.GetNamePersistant();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Intern/UI/MainMenu/Nickname.cs
@@ -0,0 +1,26 @@
+﻿// @author : Alex
+
+using UnityEngine;
+using System.Collections;
+
+namespace Extinction {
+    namespace UI {
+        public class NicknameUtil : MonoBehaviour {
+            private static string NICKNAME_KEY_PREFERENCE = "ExtinctionNickname";
+
+            public static string GetNamePersistant() {
+                Debug.Log(PlayerPrefs.GetString(NICKNAME_KEY_PREFERENCE).ToString());
+                return PlayerPrefs.GetString(NICKNAME_KEY_PREFERENCE, "Extinction Guest " + (int)(Random.value * 100));
+            }
+
+            public static void SetNamePersistant(string nickname) {
+                PlayerPrefs.SetString(NICKNAME_KEY_PREFERENCE, nickname);
+            }
+
+            public void SetNamePersistantScript(string nickname) {
+                Debug.Log(nickname);
+                SetNamePersistant(nickname);
+            }
+        }
+    }
+}
