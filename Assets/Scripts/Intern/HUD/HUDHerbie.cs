e998f7de72f311e1c6432dc8d2e1d1a17f138d2a
florian <florian.lequertier@free.fr>
2015-12-27 23:37:38 +0100

    Continue HUDHerbie. TODO : Make ActiveSkills and merge it into HUD.

--- Assets/Scripts/Intern/HUD/HUDHerbie.cs
+++ Assets/Scripts/Intern/HUD/HUDHerbie.cs
@@ -3,12 +3,26 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+using Extinction.Enums;
+
 namespace Extinction
 {
     namespace HUD
     {
         public class HUDHerbie : MonoBehaviour
         {
+            [System.Serializable]
+            private class RobotHUDInfo {
+                public CharacterName _name;
+                public Transform _hudRef;
+            }
+
+            [SerializeField]
+            private List<RobotHUDInfo> _robotHudInfoList = new List<RobotHUDInfo>();
+
+            private Dictionary<CharacterName, Transform> _robotHudInfos = new Dictionary<CharacterName, Transform>();
+
+
             /// <summary>
             /// Reference to Herbie's miniMap on the HUD.
             /// </summary>
@@ -26,21 +40,61 @@ namespace Extinction
             [SerializeField]
             Color _unselectedColor = new Color(1, 1, 1, 0.5f);
 
+            [SerializeField]
+            int nbRobotEmplacement = 3;
+
             void Awake()
             {
                 if(_miniMap == null)
                 {
-                    _miniMap =  transform.Find("/HerbieMap");
+                    _miniMap =  transform.Find("HerbieMap");
                 }
                 if(_selectionHUD == null)
                 {
-                    _selectionHUD = transform.Find("/RobotSelection");
+                    _selectionHUD = transform.Find("RobotSelection");
+                }
+
+                //fill dictionary : 
+                _robotHudInfos.Clear();
+                for( int emplacementIndex = 0; emplacementIndex < 3; emplacementIndex++ )
+                {
+                    if( _robotHudInfoList.Count <= emplacementIndex || _robotHudInfoList[emplacementIndex]._hudRef == null )
+                    {
+                        _selectionHUD.Find( "RobotEmplacement" + emplacementIndex.ToString() );
+                    }
+                    else
+                        _robotHudInfos.Add( _robotHudInfoList[emplacementIndex]._name, _robotHudInfoList[emplacementIndex]._hudRef );
+
+                    emplacementIndex++;
+                }
+
+                //fill button for casting active skills : 
+                ActiveSkill[] allActiveSkill = GetComponentsInChildren<ActiveSkill>();
+                foreach(ActiveSkill skill in allActiveSkill)
+                {
+                    Button skillButton = skill.GetComponent<Button>();
+
+                    skillButton.onClick.AddListener([]=>{
+                        skill.BeginExecution();
+                    });
                 }
             }
 
-            void changeSelection(List<string> selectedNames)
+            void changeSelection(List<CharacterName> selectedNames)
             {
-
+                foreach(KeyValuePair<CharacterName, Transform> robotInfo in _robotHudInfos)
+                {
+                    if( selectedNames.Contains( robotInfo.Key ) )
+                    {
+                        //change the visual to show a selected color on the robot hud info
+                        robotInfo.Value.GetComponent<Image>().color = _unselectedColor;
+                    }
+                    else
+                    {
+                        //change the visual to show an unselected color on the robot hud info
+                        robotInfo.Value.GetComponent<Image>().color = _unselectedColor;
+                    }
+                }
             }
 
             void clearSelection()
