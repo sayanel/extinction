cb0fd3942417e0ea8ace01d7eefa17e1b7b89bd5
Alex <alexis.oblet@gmail.com>
2015-12-31 13:21:07 +0100

    Room list game: prefabs

---
+++ Assets/Scripts/Intern/UI/MainMenu/RoomInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Extinction {
+    namespace UI {
+        public class RoomInfo : MonoBehaviour {
+            public Text gameName;
+            public Text playerNumbers;
+            public Text startTime;
+        }
+    }
+}
