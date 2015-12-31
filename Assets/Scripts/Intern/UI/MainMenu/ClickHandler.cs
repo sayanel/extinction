11b08ce6ef5bb2a45e66b4b223e32bb5534c21d9
Alex <alexis.oblet@gmail.com>
2015-12-31 13:21:45 +0100

    Utils UI: async loading progress bar script and init main menu click handler

---
+++ Assets/Scripts/Intern/UI/MainMenu/ClickHandler.cs
@@ -0,0 +1,15 @@
+﻿// @author : Alex
+
+using UnityEngine;
+using System.Collections;
+
+namespace Extinction {
+    namespace UI {
+        public class ClickHandler : MonoBehaviour {
+
+            public void QuitGame() {
+                Application.Quit();
+            }
+        }
+    }
+}
