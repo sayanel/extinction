cbfc4d6dff839d5060fe4035626f9d9d2fdb7a80
florian <florian.lequertier@free.fr>
2016-01-05 13:26:13 +0100

    Add a concrete FXShoot. Add a prefab for an example of FX, add a FXManager to the floTest scene.

---
+++ Assets/Scripts/Intern/FX/FXderived/FXShoot.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Extinction.FX;
+using System;
+
+
+public class FXShoot : FXEvent 
+{
+
+    [SerializeField]
+    private ParticleSystem _particleSystem;
+
+    [SerializeField]
+    private Light _light;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    void Awake()
+    {
+        if( _particleSystem == null )
+            _particleSystem = GetComponent<ParticleSystem>();
+
+        if( _light == null )
+            _light = GetComponent<Light>();
+
+        if( _audioSource == null )
+            _audioSource = GetComponent<AudioSource>();
+    }
+
+    public override void On()
+    {
+        if( _particleSystem != null )
+        {
+            _particleSystem.Play();
+        }
+        if( _audioSource != null )
+        {
+            _audioSource.Play();
+        }
+    }
+
+    public override void Off()
+    {
+        if( _particleSystem != null )
+        {
+            _particleSystem.Stop();
+        }
+        if( _audioSource != null )
+        {
+            _audioSource.Stop();
+        }
+    }
+}
