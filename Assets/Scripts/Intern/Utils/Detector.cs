82c52ec3ee1bdfe5a689251b53e2173a1411aefb
florianLequertier <florian.lequertier@free.fr>
2016-01-13 17:21:10 +0100

    Add ITriggerable and Detector to Extinction.Utils namespace.

--- Assets/Scripts/Intern/Utils/Detector.cs
+++ Assets/Scripts/Intern/Utils/Detector.cs
@@ -3,44 +3,53 @@
 using UnityEngine;
 using System.Collections;
 
-/// <summary>
-/// This detector will simply warn its _target when something enter or leave one of its collider.
-/// Its _target is a ITriggerable, a simple interface to handler these kind of events.
-/// If _target isn't set in the editor, the detector will try to fill this parameter searching a ITriggerable in its parent.
-/// </summary>
-public class Detector : MonoBehaviour {
 
-    [SerializeField]
-    private string _tag;
-
-    [SerializeField]
-    private GameObject _target;
-
-    private ITriggerable _triggerableTarget;
-
-    private Collider _collider;
-
-    // Use this for initialization
-    void Start ()
+namespace Extinction
+{
+    namespace Utils
     {
-        if (_collider == null)
-            Debug.LogWarning("You have to place a collider on a GameObject which have a Detector Component ! ");
-        if (_target == null)
-            _target = transform.parent.gameObject;
 
-        _triggerableTarget = _target.GetComponent<ITriggerable>();
-	}
+        /// <summary>
+        /// This detector will simply warn its _target when something enter or leave one of its collider.
+        /// Its _target is a ITriggerable, a simple interface to handler these kind of events.
+        /// If _target isn't set in the editor, the detector will try to fill this parameter searching a ITriggerable in its parent.
+        /// </summary>
+        public class Detector : MonoBehaviour
+        {
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(_target != null)
-            _triggerableTarget.triggerEnter(other);
+            [SerializeField]
+            private string _tag;
+
+            [SerializeField]
+            private GameObject _target;
+
+            private ITriggerable _triggerableTarget;
+
+            private Collider _collider;
+
+            // Use this for initialization
+            void Start()
+            {
+                if (_collider == null)
+                    Debug.LogWarning("You have to place a collider on a GameObject which have a Detector Component ! ");
+                if (_target == null)
+                    _target = transform.parent.gameObject;
+
+                _triggerableTarget = _target.GetComponent<ITriggerable>();
+            }
+
+            void OnTriggerEnter(Collider other)
+            {
+                if (_target != null)
+                    _triggerableTarget.triggerEnter(other);
+            }
+
+            void OnTriggerExit(Collider other)
+            {
+                if (_target != null)
+                    _triggerableTarget.triggerExit(other);
+            }
+
+        }
     }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (_target != null)
-            _triggerableTarget.triggerExit(other);
-    }
-	
 }
--- Assets/Scripts/Intern/Utils/ITriggerable.cs
+++ Assets/Scripts/Intern/Utils/ITriggerable.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/// <summary>
-/// Symple interface to trigegr some functions. It is used by the Detector to call function when an object enter or leave one of its colliders.
-/// </summary>
-public interface ITriggerable {
+namespace Extinction
+{
+    namespace Utils
+    {
 
-    void triggerEnter(Collider other);
-    void triggerExit(Collider other);
+        /// <summary>
+        /// Symple interface to trigegr some functions. It is used by the Detector to call function when an object enter or leave one of its colliders.
+        /// </summary>
+        public interface ITriggerable
+        {
+
+            void triggerEnter(Collider other);
+            void triggerExit(Collider other);
+        }
+    }
 }
