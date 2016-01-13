bc4e14f85310a45e157ba4ec624157f4fa09351f
florianLequertier <florian.lequertier@free.fr>
2016-01-13 14:25:47 +0100

    add survivor implementation to the branch for testing.

---
+++ Assets/Scripts/Intern/Utils/GUISurvivorDebugger.cs
@@ -0,0 +1,40 @@
+﻿// @author : Mehdi
+
+using UnityEngine;
+using System.Collections;
+using Extinction.Characters;
+using UnityEngine.UI;
+
+namespace Extinction
+{
+    namespace Utils
+    {
+        public class GUISurvivorDebugger : MonoBehaviour
+        {
+            // ----------------------------------------------------------------------------
+            // -------------------------------- ATTRIBUTES --------------------------------
+            // ----------------------------------------------------------------------------
+
+            [SerializeField]
+            private Text _survivorStateField;
+
+            [SerializeField]
+            private Text _survivorAimingField;
+
+            [SerializeField]
+            private Survivor _survivor;
+
+            public void Update()
+            {
+                _survivorStateField.text = "State : " + _survivor.state;
+                _survivorAimingField.text = "Aiming : " + _survivor.isAiming;
+            }
+
+            // ----------------------------------------------------------------------------
+            // --------------------------------- METHODS ----------------------------------
+            // ----------------------------------------------------------------------------
+
+
+        }
+    }
+}
