6981de658f908e71fd8051d92af0c2bd0ccb9ac6
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2016-01-15 14:54:49 +0100

    Documented CameraFPS.cs & Survivor.cs

--- Assets/Scripts/Intern/Cameras/CameraFPS.cs
+++ Assets/Scripts/Intern/Cameras/CameraFPS.cs
@@ -9,32 +9,58 @@ namespace Extinction
 {
     namespace Cameras
     {
+        /// <summary>
+        /// This class controls a Unity Camera. 
+        /// It uses a Survivor Component to update it's position, rotation & FOV
+        /// </summary>
         public class CameraFPS : CameraModule
         {
             // ----------------------------------------------------------------------------
             // -------------------------------- ATTRIBUTES --------------------------------
             // ----------------------------------------------------------------------------
 
+            /// <summary>
+            /// pointer to the survivor
+            /// </summary>
             [SerializeField]
             private Survivor _survivor;
 
+            /// <summary>
+            /// pointer to the unity Camera
+            /// </summary>
             [SerializeField]
             private Camera _camera;
 
-            private float _currentFOV;
-
+            /// <summary>
+            /// The camera is set to _normalFOV when the survivor is not aiming
+            /// </summary>
             [SerializeField]
             private float _normalFOV = 60;
+
+            /// <summary>
+            /// The camera is set to _accurateFOV when the survivor is aiming
+            /// </summary>
             [SerializeField]
             private float _accurateFOV = 30;
-            [SerializeField]
-            private float _zoomTime = 1;
+
+            
+            /// <summary>
+            /// When the observed Survivor starts to aimer, this timer is launched to handle
+            /// interpolation from _normalFOV to _accurateFOV
+            /// </summary>
             [SerializeField]
             private Timer _timer;
 
+            /// <summary>
+            /// Time of the interpolation
+            /// </summary>
+            [SerializeField]
+            private float _zoomTime = 1;
+
+            private float _currentFOV;
+
             private float _targetFOV;
             private float _triggerFOV;
-
 
             private bool _zoomingIn;
             private bool _zoomingOut;
@@ -79,21 +105,38 @@ namespace Extinction
 
             }
 
+            /// <summary>
+            /// Set the field of view of the Unity Camera
+            /// </summary>
+            /// <param name="fieldOfView"></param>
             public override void setFieldOfView( float fieldOfView )
             {
                 _camera.fieldOfView = fieldOfView;
             }
 
+            /// <summary>
+            /// Set the local position of the Unity Camera
+            /// </summary>
+            /// <param name="position">The new position of the Camera</param>
             public override void setPosition( Vector3 position )
             {
-                throw new System.NotImplementedException();
+                transform.localPosition = position;
             }
 
+            /// <summary>
+            /// Inherited from CameraModule & Not Implemented for FPS Camera
+            /// </summary>
+            /// <param name="rotation"></param>
             public override void setRotation( Vector3 rotation )
             {
                 throw new System.NotImplementedException();
             }
 
+            /// <summary>
+            /// Zoom from the current FOV to a target FOV with time interpolation 
+            /// </summary>
+            /// <param name="targetFieldOfView">The target FOV</param>
+            /// <param name="time">The time of the interpolation</param>
             public override void zoom( float targetFieldOfView, float time )
             {
                 _targetFOV = targetFieldOfView;
@@ -102,7 +145,7 @@ namespace Extinction
                 _timer.start();
             }
 
-            public void zoomRoutine()
+            private void zoomRoutine()
             {
                 _currentFOV = Mathf.Lerp( _targetFOV, _triggerFOV, ( _timer.maxTime - _timer.currentTime ) / _timer.maxTime );
                 setFieldOfView( _currentFOV );
--- Assets/Scripts/Intern/Characters/Survivor.cs
+++ Assets/Scripts/Intern/Characters/Survivor.cs
@@ -9,44 +9,86 @@ namespace Extinction
 {
     namespace Characters
     {
+        /// <summary>
+        /// This class represents a survivor.
+        /// </summary>
         public class Survivor : Character
         {
             // ----------------------------------------------------------------------------
             // -------------------------------- ATTRIBUTES --------------------------------
             // ----------------------------------------------------------------------------
 
+            /// <summary>
+            /// Returns if the survivor is aiming or not
+            /// </summary>
+            public bool isAiming { get { return _aiming; } }
+
+            /// <summary>
+            /// Returns the survivor's orientation, i.e the vector which describes where he is looking at
+            /// </summary>
+            public Vector3 orientation { get { return _orientation; } }
+
+            /// <summary>
+            /// Returns the survivor's state.
+            /// It can be used for animation.
+            /// </summary>
+            public CharacterState state { get { return _state; } }
+
+            /// <summary>
+            /// Reduce or increase the survivor's lifebar
+            /// </summary>
             private float _lifeMultiplier = 1;
+
+            /// <summary>
+            /// Reduce or increase the survivor's stealth
+            /// </summary>
             private float _stealthMultiplier = 1;
+
+            /// <summary>
+            /// Reduce or increase the survivor's damages
+            /// </summary>
             private float _damageMultiplier = 1;
+
+            /// <summary>
+            /// Reduce or increase the survivor's armor
+            /// </summary>
             private float _armorMultiplier = 1;
 
+            /// <summary>
+            /// Reduce or increase the survivor's speed
+            /// </summary>
             [SerializeField]
             private float _speedMultiplier = 3;
 
+            /// <summary>
+            /// A pointer to a Unity CharacterController
+            /// The Controller Should be put in the same GameObject than this script
+            /// </summary>
             [SerializeField]
             private CharacterController _controller;
 
+            /// <summary>
+            /// A pointer to the weapon of the survivor
+            /// </summary>
+            [SerializeField]
+            private Weapon _weapon;
+
+            /// <summary>
+            /// Fake gravity used when the survivor jumps
+            /// </summary>
             [SerializeField]
             private float _gravity = 0.2f;
 
+            /// <summary>
+            /// Some kind of impulse speed when the survivor starts to jump
+            /// </summary>
             [SerializeField]
             private float _jumpImpulse = 0.4f;
 
             private float _verticalSpeed = 0;
-
             private Vector3 _speed;
             private Vector3 _orientation = Vector3.forward;
-
-            [SerializeField]
-            private Weapon _weapon;
-
             private bool _aiming = false;
-
-            public bool isAiming { get { return _aiming; } }
-
-            public Vector3 orientation { get { return _orientation; } }
-
-            public CharacterState state { get { return _state; } }
 
             // ----------------------------------------------------------------------------
             // --------------------------------- METHODS ----------------------------------
@@ -72,11 +114,20 @@ namespace Extinction
                 _speed.z = 0;
             }
 
+            /// <summary>
+            /// Remove the amount of damage from survivor's health (Inherited from Character)
+            /// </summary>
+            /// <param name="amount">The health that will be removed</param>
             public override void getDamage( int amount )
             {
                 _health -= amount * _armorMultiplier;
             }
 
+            /// <summary>
+            /// The Survivor will move it the given direction
+            /// Survivor.position += vec
+            /// </summary>
+            /// <param name="vec"></param>
             public override void move( Vector3 vec )
             {
                 _controller.Move( vec );
@@ -92,23 +143,41 @@ namespace Extinction
                 throw new System.NotImplementedException();
             }
 
+            /// <summary>
+            /// Change the player's current state to idle
+            /// </summary>
             public void idle()
             {
                 _state = CharacterState.Idle;
             }
 
+            /// <summary>
+            /// Change the player's current state to strafeLeft
+            /// Move the player left
+            /// </summary>
+            /// <param name="value"></param>
             public void strafeLeft( float value )
             {
                 _state = CharacterState.StrafeLeft;
                 horizontalMovement( value );
             }
 
+            /// <summary>
+            /// Change the player's current state to strafeRight
+            /// Move the player right
+            /// </summary>
+            /// <param name="value"></param>
             public void strafeRight( float value )
             {
                 _state = CharacterState.StrafeRight;
                 horizontalMovement( value );
             }
 
+            /// <summary>
+            /// Change the player's current state to run
+            /// Move the player front
+            /// </summary>
+            /// <param name="value"></param>
             public void run( float value )
             {
                 if(_state != CharacterState.Sprint) 
@@ -117,6 +186,11 @@ namespace Extinction
                 verticalMovement( value );
             }
 
+            /// <summary>
+            /// Change the player's current state to runBackward
+            /// Move the player back
+            /// </summary>
+            /// <param name="value"></param>
             public void runBackward( float value )
             {
                 if ( _state == CharacterState.Sprint )
@@ -128,6 +202,11 @@ namespace Extinction
                 verticalMovement( value );
             }
 
+            /// <summary>
+            /// Change the player's current state to sprint
+            /// The player will move faster as long as Sprinting Key is pressed
+            /// </summary>
+            /// <param name="value"></param>
             public void sprint(bool sprinting)
             {
                 if ( _state != CharacterState.Run && _state != CharacterState.Sprint && _state != CharacterState.Idle ) return;
@@ -136,6 +215,11 @@ namespace Extinction
                 _aiming = false;
             }
 
+            /// <summary>
+            /// Change the player's current state to jump
+            /// If the player is grounded, it creates a speed impulse
+            /// </summary>
+            /// <param name="value"></param>
             public void jump()
             {
                 if ( !_controller.isGrounded ) return;
@@ -143,26 +227,46 @@ namespace Extinction
                 _speed.y = _jumpImpulse * Time.deltaTime;
             }
 
+            /// <summary>
+            /// The survivor uses his weapon
+            /// </summary>
             public void fire()
             {
                 _weapon.fire();
             }
 
+            /// <summary>
+            /// When the survivor is aiming, he's more accurate but he cannot sprint
+            /// </summary>
+            /// <param name="aiming">True or false</param>
             public void aim( bool aiming )
             {
                 if(_state != CharacterState.Sprint) _aiming = aiming;
             }
 
+            /// <summary>
+            /// The orientation vector is rotated around X & Y axis
+            /// </summary>
+            /// <param name="verticalOrientation">The new vertical angle in degrees</param>
+            /// <param name="horizontalOrientation">The new horizontal angle in degrees</param>
             public void setOrientation( float verticalOrientation, float horizontalOrientation )
             {
                 _orientation = Quaternion.Euler( -verticalOrientation, horizontalOrientation, 0 ) * Vector3.forward;
             }
 
+            /// <summary>
+            /// Turn the GameObject around Y Axis
+            /// </summary>
+            /// <param name="angle">The rotation angle in Degrees</param>
             public override void turn( float angle )
             {
                 transform.Rotate( Vector3.up, angle );
             }
 
+            /// <summary>
+            /// Apply a Horizontal Translation to the Gameobject
+            /// </summary>
+            /// <param name="horizontalValue"></param>
             private void horizontalMovement( float horizontalValue )
             {
                 Vector3 movement = new Vector3( _orientation.z, 0, -_orientation.x ) * horizontalValue * _defaultCharacterSpeed * _speedMultiplier * Time.deltaTime;
@@ -170,6 +274,10 @@ namespace Extinction
                 _speed.x += movement.x;
                 _speed.z += movement.z;
             }
+
+            /// <summary>
+            /// Apply a Vertical Translation to the Gameobject
+            /// </summary>
             private void verticalMovement( float verticalValue )
             {
                 float multiplier = _state == CharacterState.Sprint ? 2 : 1;
@@ -183,7 +291,10 @@ namespace Extinction
                 _speed.z += movement.z;
             }
 
-            public void applyGravity()
+            /// <summary>
+            /// If the player is not grounded, applies gravity
+            /// </summary>
+            private void applyGravity()
             {
                 if ( _controller.isGrounded )
                 {
