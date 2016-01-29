65ac2e12239954c1cb201ee647e9b95cc64c355d
florian <florian.lequertier@free.fr>
2016-01-29 15:00:59 +0100

    Add a UI for selecting player, in world space.

---
+++ Assets/Scripts/Intern/UI/PlayerSelection/UITurnModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UITurnModel : MonoBehaviour 
+{
+    [SerializeField]
+    float _rotationSpeed = -1f;
+
+    [SerializeField]
+    Transform _model;
+
+    void Start()
+    {
+        //if model is null, try to find a Transform called AnchorModel, and get it's first child (normaly, it's the model we want) to initialyze _model parameter.
+        _model = transform.parent.Find( "AnchorModel" ).GetChild( 0 );
+    }
+
+    //Turn the model.
+    public void turn( float rotation )
+    {
+        if( _model != null )
+            _model.rotation = Quaternion.Euler( 0, rotation * _rotationSpeed, 0 );
+    }
+}
