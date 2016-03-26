4250af04d09a202c520b59d08dbc1b8d068dcc15
Alex <alexis.oblet@gmail.com>
2016-03-26 18:27:36 +0100

    #13 spawn positions

---
+++ Assets/Scripts/Intern/Game/SpawnPositionSurvivor.cs
@@ -0,0 +1,51 @@
+﻿// @author : Alex
+
+using UnityEngine;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+
+namespace Extinction {
+    namespace Game {
+        public class SpawnPositionSurvivor : MonoBehaviour {
+            private Dictionary<int, int> _spawnTaken = new Dictionary<int, int>();
+            private string _keyPositions = "spawnPos";
+            private PhotonView _view;
+
+            void Start() {
+                // Use rpc to stack (as a lock system)
+                _view = GetComponent<PhotonView>();
+                _view.RPC("updatePosition", PhotonTargets.All);
+            }
+
+            [PunRPC]
+            void updatePosition() {
+                if (PhotonNetwork.player.name == "herbie")
+                    return;
+
+                if (!_view.isMine)
+                    return;
+
+                if (PhotonNetwork.room.customProperties[_keyPositions] == null) {
+                    Hashtable spawnTaken = new Hashtable() { };
+                    PhotonNetwork.room.customProperties[_keyPositions] = spawnTaken;
+                }
+
+                Hashtable photonSpawnTaken = (Hashtable)PhotonNetwork.room.customProperties[_keyPositions];
+                int nbSpawns = transform.childCount;
+                int index;
+
+                while(true) {
+                    index = Random.Range(0, nbSpawns - 1);
+                    if (photonSpawnTaken[index] == null)
+                        break;
+                }
+                GameObject goSurvivor = (GameObject) PhotonNetwork.player.TagObject;
+                goSurvivor.transform.position = transform.GetChild(index).transform.position;
+
+                photonSpawnTaken.Add(index, index);
+                PhotonNetwork.room.customProperties[_keyPositions] = photonSpawnTaken;
+                Debug.Log(photonSpawnTaken.ToString());
+            }
+        }
+    }
+}
